15cb512808d95978b849bd9080c1c7a6ace46e06
dejat <tmd217@lehigh.edu>
2014-11-13 21:38:54 +0000

    updated MoreLoops

--- Lab08/MoreLoops.java
+++ Lab08/MoreLoops.java
@@ -12,7 +12,8 @@ public class MoreLoops{
   //  	scan.next(); //get rid of the junk entered by user
   //  	System.out.print("You did not enter an int; try again- ");
   //  }
-    
+        
+        System.out.print("Enter an int- ");
         do{
             scan.next();
             System.out.print("You did not enter an int; try again- ");
@@ -22,11 +23,94 @@ public class MoreLoops{
         
         
         
-     n=scan.nextInt();
+    // n=scan.nextInt();
+    //
+      //  for(int j=0;j<n && j<40;j++){
+        //	for(int k=0;k<j+1;k++){
+//       	 System.out.print('*');
+  //      	}
+    ///    	System.out.println();
+       // }
+       
+       
+        n=scan.nextInt();
+        int j=0;
+        int k=0;
+        while(j<n && j<40){
+            while(k<j+1){
+                System.out.print('*');
+                k++;}
+            System.out.println();
+            j++;}
+            
+            
+            
+//            int k=4;
+//
+//    do{
+//    	System.out.println("k="+k);
+//    	k++;
+//    }
+//     	while(k<4);
     
-        for(int j=0;j<n && j<40;j++){
-        	for(int k=0;k<j+1;k++){
-       	 System.out.print('*');
-        	}
-        	System.out.println();
+        int k1=4;
+        System.out.println("k="+k1);
+        while(k1<4){
+            System.out.println("k="+k1);
+            k1++;
         }
+        
+//        int count=0;
+//    	while(true){
+//    	switch(n){
+//    	default: System.out.println(n + " is > 5 or <1");
+//   	 break;
+//    	case 1:
+//    	case 2: System.out.print("Case 2 ");
+//   	 continue;
+//    	case 3: break;
+//    	case 4: System.out.println("Case 4");
+//    	case 5: System.out.println("Case 5");
+//   	 break;
+//    	}
+//    	count++;
+//    	if(count>10)
+//   	 break;
+//    }
+
+        for(int count=0;true;count++){
+            if(n==1){}
+            if(n==2){
+                System.out.print("Case 2 ");}
+            if(n==3){
+                break;
+            }
+            if(n==4){
+                System.out.println("Case 4");
+            }
+            if(n==5){
+                System.out.println("Case 5");
+                break;
+            }
+            if(count>10){
+                break;
+            }
+            else{
+                System.out.println(n + " is > 5 or <1");
+                break;
+            }
+            }
+            }
+            
+            }
+            
+            
+        
+
+
+
+
+
+
+            
+        
